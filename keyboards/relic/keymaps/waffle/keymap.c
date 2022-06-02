bb342dc6b8462e4a9953b43e739da7dee2edec9d
jack <0x6A73@pm.me>
2022-06-02 01:31:07 -0600

    0.17 Changes
    tap dance cleanup, encoder map, io expander cleanup
    
    crkbd fixup
    
    misc
    
    combo stuffs
    
    remove make keycode
    
    some keymaps
    
    rework minidox
    
    kyria v2 rp2040
    
    notes and rename crkbd keymaps
    
    rp2040 eeprom
    
    refactor oled code
    
    oled cleanup
    
    more oled clean up
    
    rp2040 converter & wpm graph
    
    rework last animation and wpm graph touchup
    
    crkbd oled cleanup
    
    misc
    
    fancy unicode
    
    fix unicode & other cleanup
    
    layout wrapper overhaul
    
    remove core changes
    
    some fixes & kyria converter
    
    migrate to hrm
    
    misc cleanup & remove breb
    
    lots of layout updates
    
    raw hid & misc other things
    
    readme & some other things
    
    remove unused macro `ROFL`
    add `waffle` ferris keymap
    custom color implementation
    fixup readme
    custom transport for keylogger
    speed improvements
    
    misc changes
    add xyz60 keyboard
    align userspace asm code
    fix randword
    remove a couple tap dances

--- keyboards/relic/keymaps/waffle/keymap.c
+++ keyboards/relic/keymaps/waffle/keymap.c
@@ -1,16 +1,22 @@
 #include "waffle.h"
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [_BASE] = LAYOUT_3u (
-    ALTQ,  KC_W,    KC_E,   KC_R,   KC_T,   KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_MPLY,
-    SFA,   KC_S,    KC_D,   KC_F,   KC_G,   KC_H, KC_J, KC_K,    KC_L,   SFCLN,   
-    CTLZ,  KC_X,    KC_C,   KC_V,   KC_B,   KC_N, KC_M, KC_COMM, CTLDOT, CTLSLH,
-    KC_VOLD, KC_LGUI, LWRSPC, KC_BSPC, CLIPST, KC_VOLU
+  [_BASE] = LAYOUT_waffle(
+    ___BASE1___, PNP,
+    ___BASE2___,
+    ___BASE3___,
+    KC_VOLD, KC_ESC, LWRSPC, RSEBSP, C(S(KC_V)), KC_VOLU
   ),
 
-  [_LOWER] = LAYOUT_3u (
-    ALT1,   KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    ALT0, MAKE,
-    SFEXM,  KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, SFPRN,
-    CTLESC, KC_TAB,  KC_NO,   KC_TILD, KC_GRV,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, OUT,
-    KC_MPRV, TABLE1, KC_TRNS, DEG, TABLE2, KC_MNXT
+  [_LOWER] = LAYOUT_waffle(
+    ___LOWER1___, QK_MAKE,
+    ___LOWER2___,
+    ___LOWER3___,
+    KC_TRNS, TABLE1, KC_TRNS, KC_TRNS, TABLE2, KC_TRNS
+  ),
+  [_RAISE] = LAYOUT_waffle(
+    ___RAISE1___, KC_TRNS,
+    ___RAISE2___,
+    ___RAISE3___,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   )
 };
