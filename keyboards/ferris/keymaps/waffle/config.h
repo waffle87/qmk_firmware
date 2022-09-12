b3a1164677d2f901896d78e4eb45107ed4d8f102
jack <0x6A73@pm.me>
2022-09-12 10:24:53 -0600

    fixup ferris & readme quote tap dance remove quote combo

--- keyboards/ferris/keymaps/waffle/config.h
+++ keyboards/ferris/keymaps/waffle/config.h
@@ -8,3 +8,18 @@
 #define ENABLE_RGB_MATRIX_CYCLE_LEFT_RIGHT
 #define ENABLE_RGB_MATRIX_CYCLE_SPIRAL
 #define ENABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define __ NO_PIN
+#undef DIRECT_PINS
+#undef DIRECT_PINS_RIGHT
+#define DIRECT_PINS { \
+   {D7,F7,F6,F5,F4},  \
+   {B1,B3,B2,B6,D3},  \
+   {D1,D0,D4,C6,E6},  \
+   {B4,B5,__,__,__}   \
+}
+#define DIRECT_PINS_RIGHT { \
+   {F4,F5,F6,F7,D7},        \
+   {D3,B6,B2,B3,B1},        \
+   {E6,C6,D4,D0,D1},        \
+   {B5,B4,__,__,__}         \
+}
