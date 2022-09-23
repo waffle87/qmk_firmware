8c33e457a0d4c43d891b037faa082155f5601206
jack <0x6A73@pm.me>
2022-09-23 01:34:04 -0600

    auto correct misc userspace/keyboard cleanup

--- keyboards/smoothie_split/config.h
+++ keyboards/smoothie_split/config.h
@@ -2,6 +2,6 @@
 #define VENDOR_ID 0xAE7E
 #define PRODUCT_ID 0x7373
 #define DEVICE_VER 0x0001
-#define PRODUCT smoothie split
+#define PRODUCT "smoothie split"
 #define MATRIX_ROWS 4 * 2
 #define MATRIX_COLS 12
